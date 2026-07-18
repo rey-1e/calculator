64090aee0eb69a2153093716699d0a699642240b
rey-1e <kanaderaj1216@gmail.com>
2026-07-18 16:19:09 +0530

    script.js now perfectly handles -> decimals, uneven triggering of = sign rounds off the output to two decimal digits handle common edge cases such as triggering operation function without entering numbers, repetitive clicking of multiple symmbols, etc.
    styles now contain border-radius for main container & keys
    added background-color to multiple elements

--- script.js
+++ script.js
@@ -1,31 +1,104 @@
 let keys = document.querySelector(".keys");
-let key = document.querySelector(".key");
+let key = document.querySelectorAll(".key");
 let screen = document.querySelector(".screen");
 
-let statement = "";
-key.addEventListener("click", (evt)=> {
+let num1 = "";
+let num2 = "";
+let symbol = '#';
 
-    if(evt.target.innerText == "AC") {
-        screen.innerText = "";
-    } else if(evt.target.innerText == "C") {
-        //remove last element from screen.innerText
-        screen.innerText = '0';
-    } else if(evt.target.innerText == "=") {
-        //call a function which returns the value;
+
+keys.addEventListener("click", (evt) => {
+    if (evt.target == keys) {
+        return;
+    }
+
+    if (evt.target.innerText == "AC") {
+        num1 = "";
+        num2 = "";
+        symbol = '#';
+        screen.innerText = "0";
+        return;
+    }
+    if (evt.target.innerText == "C") {
+        num1 = "";
+        num2 = "";
+        symbol = '#';
+        screen.innerText = "0";
+        return;
+    }
+
+    if (evt.target.innerText == "=") {
+        if (symbol == '#' || num2 == "") {
+            screen.innerText = "NULL";
+            num1 = "";
+            num2 = "";
+            symbol = '#';
+            return;
+        }
+
+        num1 = operate(num1, num2, symbol);
+        num2 = "";
+        symbol = '#';
+        screen.innerText = num1;
+        return;
     }
 
     //check for symbols; 
-    if(isSymbol(evt.target.innerText)) {
-        
+    if (isSymbol(evt.target.innerText)) {
+        if (num1 == "") {
+            return;
+        }
+        if (num2 != "") {
+            num1 = operate(num1, num2, symbol);
+            num2 = "";
+            symbol = evt.target.innerText;
+            screen.innerText = `${num1}${symbol}`;
+            return;
+        }
+
         symbol = evt.target.innerText;
+        screen.innerText = `${num1}${symbol}`;
+        return;
     }
 
-    //take expression number; 
-    statement += evt.target.innerText;
-
+    if (symbol == '#') {
+        num1 += evt.target.innerText;
+        screen.innerText = num1;
+    } else {
+        num2 += evt.target.innerText;
+        screen.innerText = `${num1}${symbol}${num2}`;
+    }
 
 });
 
 function isSymbol(El) {
-    return (El == '%' || El == '/' || El == '*' || El == '-' || El == '+' || El == '.');
+    return (El == '%' || El == '/' || El == '*' || El == '-' || El == '+');
+}
+
+function operate(num1, num2, symbol) {
+    let a = parseFloat(num1);
+    let b = parseFloat(num2);
+    let result;
+
+    if (symbol == '+') {
+        result = a + b;
+    } else if (symbol == '-') {
+        result = a - b;
+    } else if (symbol == '*') {
+        result = a * b;
+    } else if (symbol == '/') {
+        if (b == 0) {
+            return "awww";
+        }
+        result = a / b;
+    } else if (symbol == '%') {
+        if (b == 0) {
+            return "awww";
+        }
+        result = a % b;
+    } else {
+        return "NULL";
+    }
+
+    return Math.round(result * 100) / 100;
 }
